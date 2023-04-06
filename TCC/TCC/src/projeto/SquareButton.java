a26933a11597a5ea150cf1526b3dc5cf5480101d
krrozino <129542135+krrozino@users.noreply.github.com>
2023-04-06 20:00:24 -0300

    Add files via upload

--- TCC/TCC/src/projeto/SquareButton.java
+++ TCC/TCC/src/projeto/SquareButton.java
@@ -9,16 +9,33 @@ package projeto;
  */
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
 import javax.swing.plaf.basic.BasicButtonUI;
 
 public class SquareButton extends JButton {
+
     public SquareButton(String text) {
         super(text);
         setUI(new BasicButtonUI()); // usa a classe de interface de usuário básica do Swing
-        setBackground(Color.WHITE); // define o fundo do botão como branco
         setBorderPainted(false); // remove a borda do botão
-        setContentAreaFilled(false); // remove o fundo do botão
+    }
+    
+    public SquareButton() {
+        super();
+        setBorder(new LineBorder(Color.BLACK, 2));
+        setPreferredSize(new Dimension(50, 50));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (getModel().isPressed()) {
+            g.setColor(Color.GRAY);
+        } else {
+            g.setColor(Color.WHITE);
+        }
+        g.fillRect(0, 0, getWidth(), getHeight());
+        super.paintComponent(g);
     }
 }
